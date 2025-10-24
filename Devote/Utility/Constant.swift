21df3aba523c345a528691f094e8623d1980dc58
Ghenadie Isacenco <isacenco@gmail.com>
2025-10-24 13:35:27 +0200

    Core Data Knowledge + some changes

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Ghenadie Isacenco on 24/10/2025.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
+
